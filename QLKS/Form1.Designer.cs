d2f2388a515690c28ad1be7d1607056affb6488f
tcminh1108 <tcminh1108@gmail.com>
2021-10-04 22:28:26 +0700

    Add project files.

---
+++ QLKS/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace QLKS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbbTinhTrang = new System.Windows.Forms.ComboBox();
+            this.txtDonGia = new System.Windows.Forms.TextBox();
+            this.txtMaPhong = new System.Windows.Forms.TextBox();
+            this.lblMaPhong = new System.Windows.Forms.Label();
+            this.lblTinhTrang = new System.Windows.Forms.Label();
+            this.lblDonGia = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvThongTinPhong = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbbTinhTrang);
+            this.groupBox1.Controls.Add(this.txtDonGia);
+            this.groupBox1.Controls.Add(this.txtMaPhong);
+            this.groupBox1.Controls.Add(this.lblMaPhong);
+            this.groupBox1.Controls.Add(this.lblTinhTrang);
+            this.groupBox1.Controls.Add(this.lblDonGia);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(26, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(773, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập Thông Tin Phòng";
+            // 
+            // cbbTinhTrang
+            // 
+            this.cbbTinhTrang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbTinhTrang.FormattingEnabled = true;
+            this.cbbTinhTrang.Items.AddRange(new object[] {
+            "Trống",
+            "Đã Thuê"});
+            this.cbbTinhTrang.Location = new System.Drawing.Point(524, 35);
+            this.cbbTinhTrang.Name = "cbbTinhTrang";
+            this.cbbTinhTrang.Size = new System.Drawing.Size(176, 28);
+            this.cbbTinhTrang.TabIndex = 2;
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDonGia.Location = new System.Drawing.Point(167, 84);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(176, 27);
+            this.txtDonGia.TabIndex = 1;
+            // 
+            // txtMaPhong
+            // 
+            this.txtMaPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaPhong.Location = new System.Drawing.Point(167, 37);
+            this.txtMaPhong.Name = "txtMaPhong";
+            this.txtMaPhong.Size = new System.Drawing.Size(176, 27);
+            this.txtMaPhong.TabIndex = 1;
+            // 
+            // lblMaPhong
+            // 
+            this.lblMaPhong.AutoSize = true;
+            this.lblMaPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaPhong.Location = new System.Drawing.Point(59, 42);
+            this.lblMaPhong.Name = "lblMaPhong";
+            this.lblMaPhong.Size = new System.Drawing.Size(92, 20);
+            this.lblMaPhong.TabIndex = 0;
+            this.lblMaPhong.Text = "Mã Phòng";
+            // 
+            // lblTinhTrang
+            // 
+            this.lblTinhTrang.AutoSize = true;
+            this.lblTinhTrang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTinhTrang.Location = new System.Drawing.Point(414, 42);
+            this.lblTinhTrang.Name = "lblTinhTrang";
+            this.lblTinhTrang.Size = new System.Drawing.Size(89, 20);
+            this.lblTinhTrang.TabIndex = 0;
+            this.lblTinhTrang.Text = "Tình Trạng";
+            // 
+            // lblDonGia
+            // 
+            this.lblDonGia.AutoSize = true;
+            this.lblDonGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDonGia.Location = new System.Drawing.Point(60, 89);
+            this.lblDonGia.Name = "lblDonGia";
+            this.lblDonGia.Size = new System.Drawing.Size(77, 20);
+            this.lblDonGia.TabIndex = 0;
+            this.lblDonGia.Text = "Đơn Giá";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lvThongTinPhong);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(27, 310);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(773, 285);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông Tin Phòng";
+            // 
+            // lvThongTinPhong
+            // 
+            this.lvThongTinPhong.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvThongTinPhong.FullRowSelect = true;
+            this.lvThongTinPhong.GridLines = true;
+            this.lvThongTinPhong.HideSelection = false;
+            this.lvThongTinPhong.Location = new System.Drawing.Point(62, 41);
+            this.lvThongTinPhong.Name = "lvThongTinPhong";
+            this.lvThongTinPhong.Size = new System.Drawing.Size(638, 238);
+            this.lvThongTinPhong.TabIndex = 0;
+            this.lvThongTinPhong.UseCompatibleStateImageBehavior = false;
+            this.lvThongTinPhong.View = System.Windows.Forms.View.Details;
+            this.lvThongTinPhong.Click += new System.EventHandler(this.lvThongTinPhong_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã Phòng";
+            this.columnHeader1.Width = 170;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Đơn Giá";
+            this.columnHeader2.Width = 170;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Tình Trạng";
+            this.columnHeader3.Width = 200;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnTimKiem);
+            this.groupBox3.Controls.Add(this.btnThoat);
+            this.groupBox3.Controls.Add(this.btnXoa);
+            this.groupBox3.Controls.Add(this.txtTimKiem);
+            this.groupBox3.Controls.Add(this.btnSua);
+            this.groupBox3.Controls.Add(this.btnThem);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(26, 160);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(773, 143);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Chức Năng";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(624, 39);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(103, 36);
+            this.btnThoat.TabIndex = 1;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(449, 39);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(103, 36);
+            this.btnXoa.TabIndex = 1;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(265, 39);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(103, 36);
+            this.btnSua.TabIndex = 1;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(62, 39);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(103, 36);
+            this.btnThem.TabIndex = 1;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiem.Location = new System.Drawing.Point(63, 103);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(176, 27);
+            this.txtTimKiem.TabIndex = 1;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(265, 97);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(103, 35);
+            this.btnTimKiem.TabIndex = 3;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 702);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbbTinhTrang;
+        private System.Windows.Forms.TextBox txtDonGia;
+        private System.Windows.Forms.TextBox txtMaPhong;
+        private System.Windows.Forms.Label lblMaPhong;
+        private System.Windows.Forms.Label lblTinhTrang;
+        private System.Windows.Forms.Label lblDonGia;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lvThongTinPhong;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+    }
+}
+
---
+++ QLKS/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//dùng thư viện này để kết nối sql server
+using System.Data.SqlClient;
+
+namespace QLKS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //lưu chuỗi kết nối
+        string chuoiketnoi = @"Data Source=DESKTOP-CT5FGGC;Initial Catalog=QLKS;Integrated Security=True";
+        
+        //lưu câu lệnh truy vấn trong sql
+        string sql;
+
+        SqlConnection ketnoi;
+        SqlCommand thuchien;
+        SqlDataReader docdulieu;
+        int i;
+
+        #region chạy form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ketnoi = new SqlConnection(chuoiketnoi);
+            hienthi();
+        }
+        #endregion
+
+        #region Hiển Thị
+        public void hienthi()
+        {
+            lvThongTinPhong.Items.Clear();
+            ketnoi.Open();
+            sql = @"Select * from PHONG";
+            thuchien = new SqlCommand(sql, ketnoi);
+            docdulieu = thuchien.ExecuteReader();
+            i = 0;
+            while (docdulieu.Read())
+            {
+                lvThongTinPhong.Items.Add(docdulieu[0].ToString());
+                lvThongTinPhong.Items[i].SubItems.Add(docdulieu[1].ToString());
+                lvThongTinPhong.Items[i].SubItems.Add(docdulieu[2].ToString());
+                i++;
+            }
+            docdulieu.Close();
+            ketnoi.Close();
+        }
+        #endregion
+
+        #region Thêm
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            lvThongTinPhong.Items.Clear();
+            ketnoi.Open();
+            sql = @"insert into PHONG 
+                    values ('" + txtMaPhong.Text +"',N'" + txtDonGia.Text +"',N'" + cbbTinhTrang.Text +"')";
+            thuchien = new SqlCommand(sql, ketnoi);
+            int kq = thuchien.ExecuteNonQuery();
+            if(kq > 0)
+            {
+                MessageBox.Show("Thêm Dữ Liệu Thành Công!!!");
+            }
+            else
+            {
+                MessageBox.Show("Không Thể Thêm Dữ Liệu!!!");
+            }
+            ketnoi.Close();
+            hienthi();
+        }
+        #endregion
+
+        #region Sửa
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            lvThongTinPhong.Items.Clear();
+            ketnoi.Open();
+            sql = @"update PHONG set
+                    MaPhong = '" + txtMaPhong.Text + "', DonGia = N'" + txtDonGia.Text + "', TinhTrang = N'" + cbbTinhTrang.Text + @"'
+                    where (MaPhong = N'" + txtMaPhong.Text + @"')";
+            thuchien = new SqlCommand(sql, ketnoi);
+            //thuchien.ExecuteNonQuery();
+            int kq = thuchien.ExecuteNonQuery();
+            if (kq > 0)
+            {
+                MessageBox.Show("Sửa Dữ Liệu Thành Công!!!");
+            }
+            else
+            {
+                MessageBox.Show("Không Thể Sửa Dữ Liệu\nVui Lòng Không Sửa Mã Phòng!!!");
+            }    
+            ketnoi.Close();
+            hienthi();
+        }
+        #endregion
+
+        #region Xóa
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            lvThongTinPhong.Items.Clear();
+            ketnoi.Open();
+            sql = @"delete from PHONG where(MaPhong = '" + txtMaPhong.Text + "')";
+            thuchien = new SqlCommand(sql, ketnoi);
+            int kq = thuchien.ExecuteNonQuery();
+            if (kq > 0)
+            {
+                MessageBox.Show("Xóa Dữ Liệu Thành Công!!!");
+            }
+            else
+            {
+                MessageBox.Show("Không Thể Xửa Dữ Liệu!!!");
+            }
+            ketnoi.Close();
+            hienthi();
+        }
+        #endregion
+
+        #region Tìm Kiếm
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            lvThongTinPhong.Items.Clear();
+            ketnoi.Open();
+            sql = @"select * from PHONG where (MaPhong = '" + txtTimKiem.Text + "')";
+            thuchien = new SqlCommand(sql, ketnoi);
+            docdulieu = thuchien.ExecuteReader();
+            i = 0;
+            while (docdulieu.Read())
+            {
+                lvThongTinPhong.Items.Add(docdulieu[0].ToString());
+                lvThongTinPhong.Items[i].SubItems.Add(docdulieu[1].ToString());
+                lvThongTinPhong.Items[i].SubItems.Add(docdulieu[2].ToString());
+                i++;
+            }
+            docdulieu.Close();
+            ketnoi.Close();
+        }
+        #endregion
+
+        //thao tác của bài cũ
+        #region Bài Cũ
+        #region chuyencode
+        private void lvThongTinPhong_Click(object sender, EventArgs e)
+        {
+            txtMaPhong.Text = lvThongTinPhong.SelectedItems[0].SubItems[0].Text;
+            txtDonGia.Text = lvThongTinPhong.SelectedItems[0].SubItems[1].Text;
+            cbbTinhTrang.Text = lvThongTinPhong.SelectedItems[0].SubItems[2].Text;
+        }
+        #endregion
+        #region Thoát
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult thoat = MessageBox.Show("Bạn Có Muốn Thoát Chương Trình?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (thoat == DialogResult.Yes)
+            {
+                this.Close();
+                Application.Exit();
+            }
+        }
+        #endregion
+
+        #endregion
+    }
+}
